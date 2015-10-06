6779df431efee89356ed9e12c993fd45c53a3f17
Grant Farnsworth <grantfar@gmail.com>
2015-10-06 15:25:56 -0400

    started, doesn't work at all yet

---
+++ cis237assignment3/Droid.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237assignment3
+{
+    abstract class Droid: IDroid
+    {
+        private string material, model, color;
+        private decimal baseCost, totalCost;
+
+        public virtual void CalculateBaseCost()
+        {
+            baseCost = 2000m;
+            
+        }
+        public abstract void CalculateTotalCost();
+
+        public decimal TotalCost
+        {
+            get
+            {
+                return totalCost;
+            }
+            set
+            {
+                totalCost = value;
+            }
+        }
+    }
+}
---
+++ cis237assignment3/droidModelPrice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237assignment3
+{
+    class droidModelPrice
+    {
+        public droidModelPrice(string Model, decimal Price)
+        {
+            price = Price;
+            model = Model;
+        }
+        public decimal price;
+        public string model;
+    }
+}
